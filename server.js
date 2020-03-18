810e74807380a1e9d976a12d3cd4ee00f3af1683
Anshul Goyal <anshulgoyal97@gmail.com>
2020-03-18 22:46:18 +0530

    Added routes and models

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const connectDB = require('./config/database');
 
+const authentication = require('./routes/api/authentication');
+const post = require('./routes/api/post');
+const profile = require('./routes/api/profile');
+const user = require('./routes/api/user');
+
 const app = express();
 
 connectDB();
 
 const PORT = process.env.PORT || 5000; 
 
+//@routes Points to Routes Folder
+app.use('/api/users' , user);
+app.use('./api/posts' , post);
+app.use('./api/profiles' , profile);
+app.use('./api/auth' , authentication);
+
+app.get('/' , (req , res) => res.send('API Running'));
+
 app.listen(PORT , () => {
     console.log(`Server started on port ${PORT}`);
 });
 
-app.get('/' , (req , res) => res.send('API Running'));
+
