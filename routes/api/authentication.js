e050d090ef85cbac49e4b5995b22829bae1e67bf
Anshul Goyal <anshulgoyal97@gmail.com>
2020-03-21 12:03:21 +0530

    Added auth and sign in routes

--- routes/api/authentication.js
+++ routes/api/authentication.js
@@ -1,10 +1,74 @@
 const express = require('express');
-
+const auth = require('../../middleware/auth');
 const router = express.Router();
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+
 
 //@route   GET api/auth
-//@desc    Test Route
+//@desc    Auth  Route
 //@access  Public
-router.get('/' , (req , res) => res.send('Authentication Route'));
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route POST api/auth
+//@desc  SignIn Route
+//@access Public
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Passowrd is required').exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+    try {
+        const { email, password } = req.body;
+
+        let user = await User.findOne({ email });
+
+        // See if user exists
+        if (!user) {
+            res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        //Return jsonwebtoken
+        jwt.sign(payload, config.get('jwtSecret'),
+            { expiresIn: 360000 }, (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error: ' + error.message);
+    }
+}
+);
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/database');
-
-const authentication = require('./routes/api/authentication');
+const auth = require('./routes/api/authentication');
 const post = require('./routes/api/post');
 const profile = require('./routes/api/profile');
 const user = require('./routes/api/user');
@@ -18,9 +17,9 @@ const PORT = process.env.PORT || 5000;
 
 //@routes Points to Routes Folder
 app.use('/api/users' , user);
-app.use('./api/posts' , post);
-app.use('./api/profiles' , profile);
-app.use('./api/auth' , authentication);
+app.use('/api/posts' , post);
+app.use('/api/profiles' , profile);
+app.use('/api/auth' , auth);
 
 app.listen(PORT , () => {
     console.log(`Server started on port ${PORT}`);
