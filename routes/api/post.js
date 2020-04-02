1b31e93673c97a383c4cd8ccf1f62e738022171f
Anshul Goyal <anshulgoyal97@gmail.com>
2020-04-02 22:57:13 +0530

    Added functionality to like and dislike posts

--- routes/api/post.js
+++ routes/api/post.js
@@ -99,4 +99,90 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+//@route   PUT api/post/like/:id
+//@desc    Like a post
+//@access  Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        //Check if post has already been liked
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Post Already Liked' });
+        }
+
+
+
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+
+        return res.json(post.likes);
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send('Server Error');
+    }
+});
+
+//@route   PUT api/post/unlikelike/:id
+//@desc    unLike a post
+//@access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        //Check if post has already been liked
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: 'Post has not yet been Liked' });
+        }
+
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+
+        await post.save();
+
+        return res.json(post.likes);
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send('Server Error');
+    }
+});
+
+//@route   POST api/posts/comment/:id
+//@desc    Comment on a post
+//@access  Private
+router.post('/comment/:id', [auth, [
+    check('text', 'Text is Required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+        const user = await User.findById(req.user.id).select('-password');
+
+        const post = await Post.findById(req.params.id);
+
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        }
+
+        post.comments.unshift(newComment);
+
+        await post.save();
+
+        return res.json(post.comments);
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router
