14675ba66e5607b084a508ee16172038f8e64038
Anshul Goyal <anshulgoyal97@gmail.com>
2020-04-11 22:51:48 +0530

    Added createProfile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -16,6 +16,41 @@ export const getCurrentProfile = () => async dispatch => {
         dispatch({
             type: PROFILE_ERROR,
             payload: { message: error.response.statusText, status: error.response.status }
-        })
+        });
     }
-}
+};
+
+//Create or update a User Profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res = await axios.post('/api/profile', formData, config);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+        if (!edit) {
+            history.push('/dashboard');
+        }
+    } catch (error) {
+
+        const errors = error.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.message, 'danger')));
+        }
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { message: error.response.statusText, status: error.response.status }
+        });
+    }
+};
