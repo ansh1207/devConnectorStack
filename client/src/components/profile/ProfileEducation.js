713a4c62239b15caf16f0f4cd6f0d001e3d02016
Anshul Goyal <anshulgoyal97@gmail.com>
2020-04-15 12:26:59 +0530

    Added Github Profile component

--- client/src/components/profile/ProfileEducation.js
+++ client/src/components/profile/ProfileEducation.js
@@ -32,7 +32,7 @@ const ProfileEducation = ({ education: {
 }
 
 ProfileEducation.propTypes = {
-    education: PropTypes.array.isRequired
+    education: PropTypes.object.isRequired
 }
 
 export default ProfileEducation
