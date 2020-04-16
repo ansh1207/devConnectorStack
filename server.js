60fb14b3fc21e53be438239b205c5bb978051a80
Anshul Goyal <anshulgoyal97@gmail.com>
2020-04-17 01:45:16 +0530

    Added config for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const auth = require('./routes/api/authentication');
 const post = require('./routes/api/post');
 const profile = require('./routes/api/profile');
 const user = require('./routes/api/user');
+const path = require('path');
 
 const app = express();
 
@@ -11,17 +12,29 @@ const app = express();
 connectDB();
 
 //Init MiddleWare
-app.use(express.json({extended : false}));
+app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000; 
+
 
 //@routes Points to Routes Folder
-app.use('/api/users' , user);
-app.use('/api/posts' , post);
-app.use('/api/profile' , profile);
-app.use('/api/auth' , auth);
+app.use('/api/users', user);
+app.use('/api/posts', post);
+app.use('/api/profile', profile);
+app.use('/api/auth', auth);
 
-app.listen(PORT , () => {
+//Serve Static Assests in production
+if (process.env.NODE_ENV === 'production') {
+    //Set Static Folder
+    app.use(express.static('client-build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
 
